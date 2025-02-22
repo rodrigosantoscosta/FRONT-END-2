07be64a9ca156dbdabeaf419b76bab24c3b3d881
rodrigosantoscosta <rodrigosc1025@gmail.com>
2025-02-21 21:03:56 -0300

    Aula 02

---
+++ aula_02.js
@@ -0,0 +1,15 @@
+var nome = 'Pedro';
+
+if(nome){
+    console.log('Verdade');
+}
+else{
+    console.log('Mentira');
+}
+
+function calcular(){
+    var nome = 'Joao';
+    console.log('Meu nome é ' + nome);
+}
+
+calcular();
