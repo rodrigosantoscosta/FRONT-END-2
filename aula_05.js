6bac8918de90897a1c4fca606d72e54710070bb6
rodrigosantoscosta <rodrigosc1025@gmail.com>
2025-03-21 20:54:40 -0300

    aula_05

---
+++ aula_05.js
@@ -0,0 +1,30 @@
+
+// const NUMEROS  = [1,4,5,7,8]
+// NUMEROS[0] = 90;
+// console.log(NUMEROS)
+// function soma(a,b){
+//     arguments[0] = 4;
+//     let soma = a + b;
+//     return soma;
+// }
+
+// console.log(soma(5,2));
+
+// let stringvazia = '';
+
+// if (stringvazia){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+
+
+
+// let nomes = ["ana", "paulo", "pedro"];
+
+// console.log(nomes.map((nomes) => nomes.toUpperCase));
+
+const materials = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+
+console.log(materials.map((material) => material.length));
