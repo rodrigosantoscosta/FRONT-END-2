710fed6973b0b5317f587fa84ee15439d8f4a7d5
rodrigosantoscosta <rodrigosc1025@gmail.com>
2025-03-28 23:12:53 -0300

    Corrigida função desconto

--- Atividade_04/atividade_04.js
+++ Atividade_04/atividade_04.js
@@ -5,14 +5,25 @@ function calcularTotal(precoUnitario, quantidadeComprada){
 function aplicarDesconto(valorTotal){
     if (valorTotal > 100.0){
         return valorTotal * 0.9;
+    }else{
+        console.log("Não é possivel aplicar o desconto! Compra abaixo de R$100,00")
+        return valorTotal;
+    }
+}
+
+function temDesconto(valorTotal){
+    if(valorTotal> 100.0){
+        return true;
+    }else{
+        return false;
     }
 }
 
 function exibirResumo(preco, quantidade){
     let total = calcularTotal(preco, quantidade);
     totalComDesconto = aplicarDesconto(total);
-    window.alert("Preço unitário: R$" + preco.toFixed(2) + " Quantidade: " + quantidade +"\nTotal sem desconto: R$" 
-    + total.toFixed(2) +"\nTotal com desconto de 10%: R$" + totalComDesconto.toFixed(2));
+    window.alert("Preço unitário: R$" + preco.toFixed(2) + " Quantidade: " + quantidade +"\nTotal: R$" 
+    + total.toFixed(2) + (temDesconto(total) ? "\nTotal com desconto de 10%: R$" + totalComDesconto.toFixed(2) : "\n"));
 }
  
 let preco = parseFloat(prompt("Digite o preço do produto: "));
